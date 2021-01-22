ef8a4f2c17d05e4b2d42087bfa85abb3a9ea91fe
Matt <mattfsewell@gmail.com>
2021-01-22 09:10:28 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
-
+require 'pry'
+class Student < User
+ 
+    def initialize
+        @knowledge = Array.new
+    end
+    def learn(know)
+        @knowledge.push(know)
+    end
+    def knowledge
+        @knowledge
+    end
 end
